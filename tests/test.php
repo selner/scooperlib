48fd7ad2f7c99c68ee9d2d813a20c3a38395f918
Bryan Selner <bryanselner@hotmail.com>
2014-06-29 19:24:01 -0700

    Test update

--- tests/test.php
+++ tests/test.php
@@ -4,11 +4,4 @@ require_once dirname(dirname(__FILE__)) . '/bootstrap.php';
 use \Scooper\ScooperLogger;
 
 $class = new ScooperLogger();
-$class->__debug__printLine("Test", \Scooper\C__DISPLAY_ERROR__);
-
-/**
- * Created by PhpStorm.
- * User: bryan
- * Date: 6/28/14
- * Time: 7:16 PM
- */
+$class->logLine("Test", \Scooper\C__DISPLAY_ERROR__);
